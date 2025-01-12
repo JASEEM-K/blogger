b9ee67292aebdea73868ad87ce168f04451f23c1
Jaseem <muhammadjaseem791@gmail.com>
2025-01-12 07:56:46 +0000

    Feature user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document } from "mongoose";
+import { compareValue, hashValue } from "../utils/bcrypt";
+
+
+interface UserDocument extends Document {
+	_id: mongoose.Types.ObjectId,
+	username: string;
+	email: string;
+	password: string;
+	createdAt: Date;
+	updatedAt: Date;
+	comparePassword(val: string): Promise<boolean>;
+	omitPassword(): Pick<UserDocument, "_id" | "email" | "username" | "createdAt" | "updatedAt">
+}
+
+const userSchema = new mongoose.Schema<UserDocument>({
+	email: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+}, { timestamps: true })
+
+userSchema.pre("save", async function(next) {
+	if (!this.isModified("password"))
+		return next()
+	this.password === await hashValue(this.password)
+	next()
+})
+
+userSchema.methods.comparePassword = async function(val: string) {
+	return compareValue(val, this.password)
+}
+
+userSchema.methods.omitPassword = function() {
+	const user = this.toObject()
+	delete user.passwrod;
+	return user
+}
+
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema)
+export default UserModel
---
+++ backend/src/utils/bcrypt.ts
@@ -0,0 +1,6 @@
+import { compare, hash } from 'bcrypt'
+
+export const hashValue = async (value: string, saltRounds?: number) => hash(value, saltRounds || 10)
+
+export const compareValue = async (value: string, hash: string) => compare(value, hash).catch(() => false)
+
