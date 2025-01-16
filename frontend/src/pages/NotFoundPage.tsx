cc27829d0ea1cfa8ee335310f79e820b3be051d8
Jaseem <muhammadjaseem791@gmail.com>
2025-01-16 13:55:33 +0000

    added 404 page

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+
+
+
+export const NotFoundPage = () => {
+  return (
+    <div
+      className="w-screen h-screen flex justify-center items-center align-middle font-semibold"
+    >
+
+      <div className="h-40">
+        <div className="flex flex-col items-center gap-3">
+          <p className="font-bold text-4xl">
+            404
+          </p>
+          <p>
+            Page not found 😞
+          </p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
